a1ac4d8504b221d28fbfed359d3f52ae06a2b563
Shuhei Kubota <kubota.shuhei@gmail.com>
2018-03-27 18:36:31 +0900

    add build.bat

---
+++ build.bat
@@ -0,0 +1,17 @@
+set NAME=obit
+
+rem goxz -d dist -os windows -arch amd64,386 -build-ldflags "-s -w"
+
+set GOOS=windows
+set GOARCH=386
+set ZIPNAME=%NAME%_%GOOS%_%GOARCH%.zip 
+go build -ldflags "-s -w" %*
+del %ZIPNAME% 2>nul
+zip %ZIPNAME% *.exe README.txt LICENSE.txt
+
+set GOOS=windows
+set GOARCH=amd64
+set ZIPNAME=%NAME%_%GOOS%_%GOARCH%.zip 
+go build -ldflags "-s -w" %*
+del %ZIPNAME% 2>nul
+zip %ZIPNAME% *.exe README.txt LICENSE.txt
